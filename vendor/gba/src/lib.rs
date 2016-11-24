79242ade4f20b9d5f1da6fcb5fb184ff47670f39
Kevin Yap <me@kevinyap.ca>
2016-11-23 23:58:32 -0800

    Vendor gba crate from rusty-TONC
    tbelaire/rusty-TONC@3b40e9b98e747884e34ccaf972b42b8d2d0adf3e

---
+++ vendor/gba/src/lib.rs
@@ -0,0 +1,12 @@
+#![feature(intrinsics, core_intrinsics,
+           const_fn,
+           associated_consts)]
+
+#![no_std]
+
+pub mod gfx;
+pub mod input;
+pub mod memdef;
+pub mod memmap;
+pub mod tonc_stolen;
+pub mod types;
