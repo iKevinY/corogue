a289e4ed5c475383bbb238b9b0be30668dec7907
Kevin Yap <me@kevinyap.ca>
2016-11-25 20:49:06 -0800

    Clear screen when A button is pressed

--- src/main.rs
+++ src/main.rs
@@ -9,36 +9,51 @@ mod rand;
 
 pub use lang::{__aeabi_unwind_cpp_pr0, __aeabi_unwind_cpp_pr1};
 
-use gba::gfx::vid_vsync;
-use gba::gfx::{Color, Mode3};
+use gba::gfx;
+use gba::input::Keys;
 
 use rand::Rng;
 
 
 #[no_mangle]
 pub extern "C" fn main(_: i32, _: *const *const i8) -> i32 {
-    let mut m = Mode3::new();
-    let width = Mode3::WIDTH as i32;
-    let height = Mode3::HEIGHT as i32;
+    let mut m = gfx::Mode3::new();
+    let mut keys = gba::input::Input::new();
+
+    let width = gfx::Mode3::WIDTH as i32;
+    let height = gfx::Mode3::HEIGHT as i32;
 
     let mut rng = Rng::new(42);
 
     loop {
         // Only draw once per frame
-        vid_vsync();
+        gfx::vid_vsync();
 
-        // Generate a random coordinate
-        let x = rng.randint(0, width as u32) as i32;
-        let y = rng.randint(0, height as u32) as i32;
+        // Clear screen if the A button is pressed
+        keys.poll();
 
-        // Generate a random colour
-        let color = Color::rgb15(
-            rng.randint(0, 31),
-            rng.randint(0, 31),
-            rng.randint(0, 31),
-        );
+        if keys.hit(Keys::A) {
+            let black = gfx::Color::rgb15(0, 0, 0);
 
-        // Draw dot to screen
-        m.dot(x, y, color);
+            for y in 0..height {
+                for x in 0..width {
+                    m.dot(x, y, black);
+                }
+            }
+        }
+
+        // Draw 64 dots per frame
+        for _ in 0..64 {
+            let x = rng.randint(0, width as u32) as i32;
+            let y = rng.randint(0, height as u32) as i32;
+
+            let color = gfx::Color::rgb15(
+                rng.randint(0, 31),
+                rng.randint(0, 31),
+                rng.randint(0, 31),
+            );
+
+            m.dot(x, y, color);
+        }
     }
 }
